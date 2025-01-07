ed7acf5fa8e788b1a6a6a7c2cb159fffc7a45943
Saurabh Sameer <saurabhshrivastava956@gmail.com>
2025-01-08 00:26:27 +0530

    project structure setup

---
+++ configurations/src/main/java/com/saurabhsameer/configurations/Main.java
@@ -0,0 +1,7 @@
+package com.saurabhsameer.configurations;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ starter/src/main/java/com/saurabhsameer/starter/Blog.java
@@ -0,0 +1,11 @@
+package com.saurabhsameer.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blog {
+    public static void main(String[] args) {
+        SpringApplication.run(Blog.class, args);
+    }
+}
